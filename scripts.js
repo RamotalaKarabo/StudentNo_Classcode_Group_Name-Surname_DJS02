8adba2493b470a609bb59c03596991da83f905a5
Ramotala Karabo <KaraboJaz03@gmail.com>
2024-10-23 13:09:52 +0200

    fix arithmetic exceptions and errors

--- scripts.js
+++ scripts.js
@@ -5,5 +5,20 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  if(dividend.length > 0 && divider.length > 0){
+    if(divider == 0){
+      result.innerText = "Division not performed. Invalid number provided. Try again"
+    }else{ 
+      if(isNaN(dividend) && isNaN(divider)) {
+        document.write("Something critical went wrong. Please reload the page")
+      }else{
+        result.innerText = Math.floor(dividend/divider)
+      }  
+    }
+  }else{
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+  }
+
 });
+
